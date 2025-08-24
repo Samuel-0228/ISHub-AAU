ddafba4a2a9ea11fe661fb7230d90e40256ef6a8
Samuel <samuaelyeshambel@gmail.com>
2025-08-24 17:47:12 +0300

    Update taskroutes.js

---
+++ task_manager/Routes/taskroutes.js
@@ -0,0 +1,23 @@
+// routes/taskRoutes.js: Defines API endpoints and maps them to controller methods.
+
+const express = require('express');
+const taskController = require('../controllers/taskController');
+
+const router = express.Router();
+
+// GET /tasks
+router.get('/', taskController.getAllTasks);
+
+// GET /tasks/:id
+router.get('/:id', taskController.getTaskById);
+
+// POST /tasks
+router.post('/', taskController.createTask);
+
+// PUT /tasks/:id
+router.put('/:id', taskController.updateTask);
+
+// DELETE /tasks/:id
+router.delete('/:id', taskController.deleteTask);
+
+module.exports = router;
